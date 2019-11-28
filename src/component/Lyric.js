fe81461d3cb0819f8f812c83120787368817630f
S.F <1074260090@qq.com>
2019-11-28 16:27:57 +0800

    歌词

--- src/component/Lyric.js
+++ src/component/Lyric.js
@@ -11,7 +11,18 @@ class Lyric extends Component {
             lyrics:'',
         };
     }
-    
+    secondsFormat(sec){
+        let hour = Math.floor(sec / 3600);
+        let minute = Math.floor((sec - hour * 3600) / 60);
+        let second = Math.floor(sec - hour * 3600 - minute * 60);
+        if (minute < 10) {
+            minute = "0" + minute;
+        }
+        if (second < 10) {
+            second = "0" + Math.floor(second);
+        }
+        return minute + ":" + second;
+    }
     componentDidMount(){
         console.log(store.getState());
         store.subscribe(listener);
@@ -22,14 +33,23 @@ class Lyric extends Component {
                AxiosGet('/lyric?id=' + id).then(res=>{
                     console.log(res);
                     let lrc = res.lrc.lyric
-                    
+                   let lrcArr = lrc.match(new RegExp(/\[.+?\]/, "g"))
+                   console.log(lrcArr);
                 //    console.log(lrc.replace(new RegExp('\n'), '<br>['));
                     that.setState({
-                        lyrics: lrc
+                        lyrics: lrc.replace(new RegExp(/\[.+?\]/, "g"), '')
                     })
                    let audio = document.querySelector("#music")
-                   setInterval(() => {
-                       console.log(audio.currentTime);
+                   let length = 0
+                    setInterval(() => {
+                       for (let i = 0; i < lrcArr.length; i++) {
+                           const e = lrcArr[i];
+                           if (e.slice(1, 6) === that.secondsFormat(audio.currentTime) ){
+                               console.log(e.slice(1, 6));
+                               document.querySelector(".musicLyricPre").style.transform = 'translateY(' + length + 'px)'
+                               length = length - 24.1;
+                           }
+                       }
                    }, 1000);
                 })
             }
